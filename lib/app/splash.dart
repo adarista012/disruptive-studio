fb606e00d6c78dd5db9dc50978ba50430ffbd1fb
adarista012 <ad.arista012@gmail.com>
2024-04-23 23:08:09 -0400

    add firebase auth

---
+++ lib/app/splash.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+    return Scaffold(
+      body: Center(
+        child: Text('Splash'),
+      ),
+    );
+  }
+}
