2b0f6c47e2ea7d563fc9732269f383a2e30adc6a
adarista012 <ad.arista012@gmail.com>
2024-04-23 22:48:01 -0400

    instalation firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:disruptive_studio/app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
